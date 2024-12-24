d1f7c239fa31114b2e52d8c4aac90ba1d16f65a3
tasnuva-14 <tzrinmou@gmail.com>
2024-12-25 00:47:37 +0600

    first commit

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, Docker!\n');
+  res.end('Hello from Docker!\n');
 });
 
 server.listen(PORT, () => {
